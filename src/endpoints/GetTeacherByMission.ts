0b5e3406cd289ac5182e69ef83af53d2bdb9d590
rafae <rafaelgf@gmail.com>
2021-01-21 17:15:14 -0300

    Continuação Desafios

--- src/endpoints/GetTeacherByMission.ts
+++ src/endpoints/GetTeacherByMission.ts
@@ -22,8 +22,48 @@ export const GetTeacherByMission = async(req: Request,res: Response): Promise<an
          throw new Error("Nenhum professor nesta turma.")
       }
 
+      // const specialtys = result.map((name: any) => {
+      //       name.specialty
+      //    return name.specialty
+      // })
 
-      res.status(200).send(result);
+      // const finalResult = {
+      //    "name": result[0].name,
+      //    "email": result[0].email,
+      //    "specialtys": specialtys
+      // }
+
+      // const teachers = result.reduce((obj: { [x: string]: any[]; }, {name, email, specialty}: any) => {
+      //    if (!obj[name]) obj[name] = [];
+      //    obj[name].push(specialty)
+      //    return obj
+      //  }, {});
+
+      let teachers = [];
+
+      for (let i = 0; i < result.length; i++) {
+         let sameName = false;
+         for (let j = 0; j < i; j++) {
+            if (teachers[j] && result[i].name === teachers[j].name) {
+                  teachers[j].specialtys.push(
+                     result[i].specialty
+                  )
+                  sameName = true;
+                  break;
+            }
+         }
+         if (!sameName) {
+            teachers.push({
+                  name: result[i].name,
+                  email: result[i].email,
+                  specialtys: [
+                     result[i].specialty
+                  ]
+            })
+         }
+      }
+     
+      res.status(200).send(teachers);
    } catch (err) {
      res.status(errorCode).send({
        message: err.message
